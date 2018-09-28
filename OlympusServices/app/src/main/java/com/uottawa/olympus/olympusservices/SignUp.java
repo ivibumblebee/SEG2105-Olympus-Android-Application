efb585d64a377d0afeb65452ba574e662cc4056f
IvanaE <ebivibe@users.noreply.github.com>
2018-09-28 18:09:05 -0400

    added sign up page, still working on drop down menu

---
+++ OlympusServices/app/src/main/java/com/uottawa/olympus/olympusservices/SignUp.java
@@ -0,0 +1,16 @@
+package com.uottawa.olympus.olympusservices;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+    public static void main(String[] args){
+
+    }
+}
