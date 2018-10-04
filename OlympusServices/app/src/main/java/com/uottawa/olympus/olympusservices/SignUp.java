ef9a2a95301d4b2babc28d5f2989edeae7d3b2b2
IvanaE <ebivibe@users.noreply.github.com>
2018-10-04 13:23:53 -0400

    added all pages, using premade components

--- OlympusServices/app/src/main/java/com/uottawa/olympus/olympusservices/SignUp.java
+++ OlympusServices/app/src/main/java/com/uottawa/olympus/olympusservices/SignUp.java
@@ -2,13 +2,22 @@ package com.uottawa.olympus.olympusservices;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.jaredrummler.materialspinner.MaterialSpinner;
+import android.support.design.widget.Snackbar;
 
 public class SignUp extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        MaterialSpinner spinner = findViewById(R.id.RoleInput);
+        spinner.setItems("Ice Cream Sandwich", "Jelly Bean", "KitKat", "Lollipop", "Marshmallow");
+        spinner.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener<String>() {
+
+            @Override public void onItemSelected(MaterialSpinner view, int position, long id, String item) {
+                Snackbar.make(view, "Clicked " + item, Snackbar.LENGTH_LONG).show();
+            }
+        });
     }
     public static void main(String[] args){
 
--- OlympusServices/app/src/main/java/com/uottawa/olympus/olympusservices/Welcome.java
+++ OlympusServices/app/src/main/java/com/uottawa/olympus/olympusservices/Welcome.java
@@ -2,16 +2,12 @@ package com.uottawa.olympus.olympusservices;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.content.Intent;
-import android.view.View;
 
-public class Main extends AppCompatActivity {
+public class Welcome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_welcome);
     }
-
 }
