9d2b3e865543cdacff4bbe169fcfee34ebb79955
IvanaE <ebivibe@users.noreply.github.com>
2018-10-05 11:57:32 -0400

    created class files for UserType, User, ServiceProvider, Admin, Service

---
+++ OlympusServices/app/src/main/java/com/uottawa/olympus/olympusservices/User.java
@@ -0,0 +1,4 @@
+package com.uottawa.olympus.olympusservices;
+
+public class User extends UserType {
+}
---
+++ OlympusServices/app/src/main/java/com/uottawa/olympus/olympusservices/UserType.java
@@ -0,0 +1,12 @@
+package com.uottawa.olympus.olympusservices;
+
+public class UserType {
+    String firstname;
+    String lastname;
+    String username;
+    String password;
+
+    UserType(){
+
+    }
+}
