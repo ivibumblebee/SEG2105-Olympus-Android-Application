efe7404b43c4687350f18ee86208cc10523ee057
IvanaE <ebivibe@users.noreply.github.com>
2018-11-01 14:40:25 -0400

    fixed ui elements

--- OlympusServices/app/src/main/java/com/uottawa/olympus/olympusservices/ServicesList.java
+++ OlympusServices/app/src/main/java/com/uottawa/olympus/olympusservices/ServicesList.java
@@ -1,11 +1,15 @@
 package com.uottawa.olympus.olympusservices;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
@@ -39,7 +43,7 @@ public class ServicesList extends AppCompatActivity implements NewServiceDialogF
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        mAdapter = new MyAdapter(services);
+        mAdapter = new MyAdapter(services, this);
         mRecyclerView.setAdapter(mAdapter);
 
 
@@ -86,6 +90,70 @@ public class ServicesList extends AppCompatActivity implements NewServiceDialogF
         dbHelper.deleteService(name);
         dialog.dismiss();
         this.recreate();
+    }
+
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ServicesHolder> {
+
+        private Service[] services;
+        private Context context;
+
+        // Provide a reference to the views for each data item
+        // Complex data items may need more than one view per item, and
+        // you provide access to all the views for a data item in a view holder
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        public MyAdapter(Service[] services, Context context) {
+            this.services = services;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @NonNull
+        @Override
+        public ServicesHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.list_item, parent, false);
+            ServicesHolder vh = new ServicesHolder(v);
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(ServicesHolder holder, int position) {
+            Service service = services[position];
+            holder.name.setText(service.getName());
+            holder.rate.setText(""+service.getRate());
+
+
+
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return services.length;
+        }
+
+        class ServicesHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+            TextView name;
+            TextView rate;
+
+            public ServicesHolder(View row){
+                super(row);
+                name = row.findViewById(R.id.Name);
+                rate = row.findViewById(R.id.Rate);
+                row.setOnClickListener(this);
+            }
+            @Override
+            public void onClick(View view) {
+                editService(view);
+
+            }
+
+
+        }
+
+
     }
 
 }
