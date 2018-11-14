4ed5fca3ab058790b3f4a2cb50150b0aa3197b89
Batuhan Basoglu <bbaso079@uottawa.ca>
2018-11-14 12:45:59 -0500

    Added 2 Test Cases

---
+++ OlympusServices/app/src/test/java/com/uottawa/olympus/olympusservices/ServiceProviderTest2.java
@@ -0,0 +1,16 @@
+package com.uottawa.olympus.olympusservices;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ServiceProviderTest2 {
+
+    @Test
+    public void testAvailabilities() {
+    }
+
+    @Test
+    public void testOtherInstances() {
+    }
+}
